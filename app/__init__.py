07584013ef59a3eb8cca302f9d6508045532e743
YishunXu <xys05@outlook.com>
2019-07-28 15:44:41 +0800

    2.0version

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from datetime import timedelta
+
+app = Flask(__name__)
+
+# 设置静态文件缓存过期时间
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+
+from app import routes
---
+++ app/models.py
@@ -0,0 +1,18 @@
+class List(object):
+    Dir = {
+        '课程体系介绍': '',
+        'LogiSim工具': '/File/logisim',
+        'Verilog语法': '/File/verilog',
+        'FPGA原理': '/File/fpga',
+        'Nexys4DDR开发板': '/File/IO',
+        'Vivado安装及使用': '/File/vivado',
+        'TestBench编写及仿真': '/File/testbench',
+        '基本逻辑门（与、或、非、异或等）': '/File/basic',
+        '简单组合逻辑电路（如选择器、译码器、加法器）': '/File/SLC',
+        '复杂组合逻辑电路（如多位全加器等）': '/File/CLC',
+        '简单时序逻辑电路（锁存器、触发器等）': '/File/STC',
+        '复杂时序逻辑电路（如计数器、秒表等）': '/File/CT',
+        '存储器': '/File/memory',
+        '有限状态机': '/File/FSM',
+    }
+
