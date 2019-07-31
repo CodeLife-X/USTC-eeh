3941c70109dfca31d349fbcef78effea4090f87e
YishunXu <xys05@outlook.com>
2019-07-31 19:55:46 +0800

    整合到码云

---
+++ app/models.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+class List(object):
+    Dir = [
+        ['课程介绍', '/File/intro', '课程体系的介绍'],
+        ['LogiSim', '/File/logisim', 'Logisim工具介绍'],
+        ['Verilog语法', '/File/verilog', '语法初步入门'],
+        ['FPGA原理', '/File/fpga', 'FPGA原理介绍'],
+        ['Nexys4DDR开发板', '/File/IO', '开发板介绍'],
+        ['Vivado介绍', '/File/vivado', 'Vivado安装及使用'],
+        ['TestBench编写及仿真', '/File/testbench', 'TestBench编写及仿真'],
+        ['基本逻辑门', '/File/basic', '与门 或门 非门 异或门等'],
+        ['简单组合逻辑电路', '/File/SLC', '选择器 译码器 加法器等'],
+        ['复杂组合逻辑电路', '/File/CLC', '多位全加器等'],
+        ['简单时序逻辑电路', '/File/STC', '锁存器 触发器等'],
+        ['复杂时序逻辑电路', '/File/CTC', '计数器 秒表等'],
+        ['存储器', '/File/memory', '存储器介绍'],
+        ['有限状态机', '/File/FSM', 'FSM简介']
+    ]
+
+# class List(object):
+#     Dir = [
+#         {'课程体系介绍': '/File/intro'},
+#         {'LogiSim工具': '/File/logisim'},
+#         {'Verilog语法': '/File/verilog'},
+#         {'FPGA原理': '/File/fpga'},
+#         {'Nexys4DDR开发板': '/File/IO'},
+#         {'Vivado安装及使用': '/File/vivado'},
+#         {'TestBench编写及仿真': '/File/testbench'},
+#         {'基本逻辑门（与、或、非、异或等）': '/File/basic'},
+#         {'简单组合逻辑电路（如选择器、译码器、加法器）': '/File/SLC'},
+#         {'复杂组合逻辑电路（如多位全加器等）': '/File/CLC'},
+#         {'简单时序逻辑电路（锁存器、触发器等）': '/File/STC'},
+#         {'复杂时序逻辑电路（如计数器、秒表等）': '/File/CTC'},
+#         {'存储器': '/File/memory'},
+#         {'有限状态机': '/File/FSM'}
+#     ]
+
---
+++ app/routes.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from flask import render_template
+from app import app
+from app.models import List
+
+
+@app.route('/')
+@app.route('/File/intro')
+def index():
+    dirl = List().Dir
+    return render_template('File/intro.html',
+                           dir=dirl)
+
+
+@app.route('/File/logisim')
+def logisim():
+    dirl = List().Dir
+    return render_template('File/logisim.html',
+                           dir=dirl)
+
+
+@app.route('/File/verilog')
+def verilog():
+    dirl = List().Dir
+    return render_template('File/verilog.html',
+                           dir=dirl)
+
+
+@app.route('/File/fpga')
+def fpga():
+    dirl = List().Dir
+    return render_template('File/fpga.html',
+                           dir=dirl)
+
+
+@app.route('/File/IO')
+def IO():
+    dirl = List().Dir
+    return render_template('File/IO.html',
+                           dir=dirl)
+
+
+@app.route('/File/vivado')
+def vivado():
+    dirl = List().Dir
+    return render_template('File/verilog.html',
+                           dir=dirl)
+
+
+@app.route('/File/testbench')
+def testbench():
+    dirl = List().Dir
+    return render_template('File/verilog.html',
+                           dir=dirl)
+
+
+@app.route('/File/basic')
+def basic():
+    dirl = List().Dir
+    return render_template('File/basic.html',
+                           dir=dirl)
+
+
+@app.route('/File/SLC')
+def SLC():
+    dirl = List().Dir
+    return render_template('File/SLC.html',
+                           dir=dirl)
+
+
+@app.route('/File/CLC')
+def CLC():
+    dirl = List().Dir
+    return render_template('File/CLC.html',
+                           dir=dirl)
+
+
+@app.route('/File/STC')
+def STC():
+    dirl = List().Dir
+    return render_template('File/STC.html',
+                           dir=dirl)
+
+
+@app.route('/File/CTC')
+def CTC():
+    dirl = List().Dir
+    return render_template('File/CTC.html',
+                           dir=dirl)
+
+
+@app.route('/File/memory')
+def memory():
+    dirl = List().Dir
+    return render_template('File/memory.html',
+                           dir=dirl)
+
+
+@app.route('/File/FSM')
+def FSM():
+    dirl = List().Dir
+    return render_template('File/FSM.html',
+                           dir=dirl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
